ddbd0785b61db44fa7e8bf8b081331ffdac052e8
Valerie MacDougall <vmacdougall@Valeries-MacBook-Pro.local>
2018-03-08 15:24:11 -0500

    working on the details

--- wine_frontend/src/components/WineDetail.js
+++ wine_frontend/src/components/WineDetail.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class WineDetail extends React.Component {
+
+    fieldEmpty = () => {
+        return (
+            <li>{this.props.winechoice.image_thumb_url}</li>
+            <li>{'Name:' + this.props.winechoice.name}</li>
+            <li>{'Producer Name:' + this.props.winechoice.producer_name}</li>
+            <li>{'$' + (this.props.winechoice.price_in_cents)/100}</li>
+            <li>{this.props.winechoice.style}</li>
+            <li>{this.props.winechoice.tasting_note}</li>
+            <li>{this.props.winechoice.serving_suggestion}</li>
+        )
+    }
+
+
     render(){
         return(
-            <h1> Wine Detail </h1>
+            <h1> Wine Details </h1>
+            <ul>
+                
+
+            </ul>
         )
     };
 }
