4f9fdfc63f15a976da274df5aaa8c5e2c5dc563c
Valerie MacDougall <vmacdougall@Valeries-MacBook-Pro.local>
2018-03-09 11:46:19 -0500

    we're doneeeeeeeeee

--- wine_frontend/src/components/WineDetail.js
+++ wine_frontend/src/components/WineDetail.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const style={
     marginTop: "3%",
-    contentAlign:'center'
+    textAlign:'left'
 }
 const imgSize={
-    width: "35%"
+    width: "15%",
+    float: 'left',
+    margin: '3%',
+    marginTop: '0%'
+}
+const moveName = {
+    marginLeft: '3%'
 }
 class WineDetail extends React.Component {
     goBackPage = () => {
@@ -18,10 +24,9 @@ class WineDetail extends React.Component {
             if (wineId === this.props.winechoice[i].id){
                 return (
                     <div style={style}>
-                    <h1>{'Name: ' + this.props.winechoice[i].name}</h1>
+                    <h1 style={moveName}>{ this.props.winechoice[i].name}</h1>
                         <img src={this.props.winechoice[i].image_url} style={imgSize} alt="Loading"/>
                         <ul>
-                            <li>{'Name: ' + this.props.winechoice[i].name}</li>
                             <li>{'Producer Name: ' + this.props.winechoice[i].producer_name}</li>
                             <li>{'$ ' + (this.props.winechoice[i].price_in_cents)/100}</li>
                             <li>{'Style: ' + this.props.winechoice[i].style}</li>
@@ -29,6 +34,7 @@ class WineDetail extends React.Component {
                             <li>{'Food Pairing Suggestion: ' + this.props.winechoice[i].serving_suggestion}</li>
                             <li>{`Quantity in Stock ` + Number(this.props.winechoice[1].quantity)}</li>
                         </ul>
+                        <button onClick={this.goBackPage} type='back' name='back'>Go Back to Wine List</button>
                     </div>
                 )
             }
@@ -38,7 +44,6 @@ class WineDetail extends React.Component {
         return(
             <div>
                 {this.fieldEmpty()}
-                <button onClick={this.goBackPage} type='back' name='back'>Go Back to Wine List</button>
             </div>
         )
     };
